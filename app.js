09602fa1e0579bff286373b45d9b2ac135ebb99a
David Oh <davidoh72@gmail.com>
2022-01-04 21:35:02 +0900

    22.01.04

--- app.js
+++ app.js
@@ -7,5 +7,31 @@ alert(greetings + " my name is " + name);
 name = "HJ :)";
 console.log(greetings + " my name is " + name);
 
+arr = [1, 2, 3, 4, 5, "yes"];
+arr.push(name);
+console.log(arr);
+alert(arr);
+
+const person = {
+  name: "kevin",
+  gender: "male",
+  age: 32,
+  muscular: false,
+  saySomething: function (speech) {
+    tmp = "Hi so last weekend was awesome! you are not gonna believe it! so..";
+    console.log(tmp);
+    alert(tmp);
+  },
+};
+person.saySomething();
+console.log(
+  "person's name: " + person.name + "\nperson's gender: " + person.gender
+);
+person.gender = "female";
+console.log(
+  "can you change const field member? yes.. the person's gender is now : " +
+    person.gender
+);
+
 greetings = "what's up";
 console.log(greetings + "can const be changed? no");
