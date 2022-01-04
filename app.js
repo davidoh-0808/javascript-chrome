0cb61ddb27c5d23d6e7a9284f657efa75ba2246f
David Oh <davidoh72@gmail.com>
2022-01-04 15:55:13 +0900

    22.01.04

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-console.log("hi");
-alert("hi");
+const greetings = "hi";
+let name = "david";
+
+console.log(greetings + " my name is " + name);
+alert(greetings + " my name is " + name);
+
+name = "HJ :)";
+console.log(greetings + " my name is " + name);
+
+greetings = "what's up";
+console.log(greetings + "can const be changed? no");
