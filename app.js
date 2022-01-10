eb45d97b76fb30ca2251251a3fb37d24212b2119
David Oh <davidoh72@gmail.com>
2022-01-10 22:07:49 +0900

    22.01.10

--- app.js
+++ app.js
@@ -1,53 +1,9 @@
-document.title = "chnaged by javascript";
-const title = document.getElementById("change-title");
-title.innerHTML = "changing the title with javascript!";
-console.dir(title);
-console.log("the title content is " + title.innerHTML);
-console.log("the class is " + title.className);
+const loginButton = document.querySelector("#login-form button");
 
-// use getElementsByID or getElementByTagName()
-const paragraph = document.getElementsByTagName("h4");
-console.log(paragraph[0].innerText);
-// or use querySelector()
+function onLoginBtnClick() {
+  //   loginInput.value;
+  const username = loginInput.nodeValue;
+  console.log(username);
+}
 
-const p = document.querySelector(".paragraph h4"); //querySelectorAll
-// console.log(p.textContent.trim());
-const pContent = p.textContent.trim();
-
-// Event Listener
-p.addEventListener("click", handleClick);
-
-/*
-p.addEventListener("copy", handleCopy);
-p.addEventListener("mouseenter", handleMouseEnter);
-p.addEventListener("mouseleave", handleMouseLeave);
-window.addEventListener("resize", handleWindowResize);
-
-function handleCopy() {
-  alert(`${p.innerText} copied!`);
-}
-function handleMouseEnter() {
-  p.style.color = "black";
-  p.innerText = "paragraph";
-  console.log("The mouse has entered the element");
-}
-function handleMouseLeave() {
-  p.style.color = "black";
-  p.innerText = "mouse has left the text!";
-  console.log("The mouse has left the element");
-}
-function handleWindowResize() {
-  document.body.style.backgroundColor = "orange";
-  console.log(window.event);
-}
-*/
-function handleClick() {
-  let condition = "clicked";
-  if (p.className === condition) {
-    p.className = "";
-    p.innerText = "paragraph";
-  } else {
-    p.className = condition;
-    p.innerText = "text is clicked";
-  }
-}
+loginButton.addEventListener("click", onLoginBtnClick);
