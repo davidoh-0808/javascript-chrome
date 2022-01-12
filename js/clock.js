3b40298b85871c7b7295a23ff5f08c4e21501c7e
David Oh <davidoh72@gmail.com>
2022-01-12 18:25:11 +0900

    22.01.12

---
+++ js/clock.js
@@ -0,0 +1,19 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours());
+  const minutes = String(date.getMinutes());
+  const seconds = String(date.getSeconds());
+  clock.innerHTML = `${hours.padStart(2, "0")}:${minutes.padStart(
+    2,
+    "0"
+  )}:${seconds.padStart(2, "0")}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
+
+let aString = "12301230123";
+aString = parseInt(aString);
+console.log(typeof aString);
