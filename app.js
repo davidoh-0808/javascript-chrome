65c7dc588ef4c9f6b3fceb11b65a7026f52e0b69
David Oh <davidoh72@gmail.com>
2022-01-12 14:50:46 +0900

    22.01.12

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const anchorLink = document.querySelector("#login-form a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+function handleClickLink(event) {
+  event.preventDefault();
   console.log(event);
-  console.log(loginInput.value);
+  console.dir(event);
+}
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
+anchorLink.addEventListener("click", handleClickLink);
