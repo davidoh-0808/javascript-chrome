f19ec9751a0b9970fa46d64fe37d365109dc06b5
David Oh <davidoh72@gmail.com>
2022-01-14 13:34:37 +0900

    22.01.14

---
+++ js/todo.js
@@ -0,0 +1,29 @@
+toDoForm = document.getElementById("todo-form");
+toDoInput = document.querySelector("#todo-form input");
+toDoList = document.getElementById("todo-list");
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function paintToDo(newToDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const button = document.createElement("button");
+  span.innerText = newToDo;
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newToDo = toDoInput.value;
+  toDoInput.value = "";
+  paintToDo(newToDo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
